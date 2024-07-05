3104b14965a112f919caeefa1d6b250743c62f9f
Manshilin Oleg <manshilin.o@gmail.com>
2024-07-06 01:38:43 +0300

    Added tests for task 554

--- tests/task_554_test.py
+++ tests/task_554_test.py
@@ -1,3 +1,16 @@
+from pathlib import Path
+
+import numpy as np
+
+from src.task_554 import main, parser
+
+
 def test_task_554():
-    assert True
-    pass
+    path = Path("./tests/data/554/test1.txt")
+    assert np.all(main(*parser(path)) == np.array([[6, 9]]))
+    path = Path("./tests/data/554/test2.txt")
+    assert np.all(main(*parser(path)) == np.array([[3, 6], [6, 3]]))
+    path = Path("./tests/data/554/test3.txt")
+    assert np.all(main(*parser(path)) == np.array([[-2, 2, -2, 2], [3, -3, 3, -3]]))
+    path = Path("./tests/data/554/test4.txt")
+    assert np.all(main(*parser(path)) == np.array([[4]]))
