98b5907ad43f93d7553fa502cf61a97ce338f6be
Manshilin Oleg <manshilin.o@gmail.com>
2024-07-06 12:29:37 +0300

    Added tests for data parsing for task 554

--- tests/task_554_test.py
+++ tests/task_554_test.py
@@ -5,6 +5,28 @@ import numpy as np
 from src.task_554 import main, parser
 
 
+def test_parser_554():
+    path = Path("./tests/data/554/test1.txt")
+    arr, ker = parser(path)
+    assert np.all(arr == np.array([[1, 2, 3], [2, 3, 4]]))
+    assert np.all(ker == np.array([[1, 0], [1, 1]]))
+
+    path = Path("./tests/data/554/test2.txt")
+    arr, ker = parser(path)
+    assert np.all(arr == np.array([[1, 2], [2, 1]]))
+    assert np.all(ker == np.array([[3]]))
+
+    path = Path("./tests/data/554/test3.txt")
+    arr, ker = parser(path)
+    assert np.all(arr == np.array([[1, 0, 1, 0, 1], [0, 1, 0, 1, 0], [2, 0, 2, 0, 2]]))
+    assert np.all(ker == np.array([[-1, 1], [1, -1]]))
+
+    path = Path("./tests/data/554/test4.txt")
+    arr, ker = parser(path)
+    assert np.all(arr == np.array([[1, 2], [3, 4]]))
+    assert np.all(ker == np.array([[0, 0], [0, 1]]))
+
+
 def test_task_554():
     path = Path("./tests/data/554/test1.txt")
     assert np.all(main(*parser(path)) == np.array([[6, 9]]))
