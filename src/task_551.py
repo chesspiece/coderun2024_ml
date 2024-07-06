2a0d6d01adc08bd85f361c37946c7a8d6bafd776
Manshilin Oleg <manshilin.o@gmail.com>
2024-07-07 02:11:15 +0300

    Made naive implementtaion of algorithm This version is unable to comform to time requirenments of the task

--- src/task_551.py
+++ src/task_551.py
@@ -4,6 +4,16 @@ from pathlib import Path
 import numpy.typing as npt
 
 
+def gcd(a: int, b: int) -> int:
+    """
+    Returns the gcd of its inputs times the sign of b if b is nonzero,
+    and times the sign of a if b is 0.
+    """
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+
 def parser(
     path: os.PathLike[str],
 ) -> tuple[npt.NDArray[np.int64], npt.NDArray[np.int64], int]:
@@ -18,7 +28,7 @@ def parser(
         h2_classes = np.array(
             [int(x) for x in f.readline().strip().split(sep=" ")]
         )  # classes choosen by second human
-        return h1_classes, h2_classes, size
+    return h1_classes, h2_classes, size
 
 
 def similarity(
@@ -27,9 +37,18 @@ def similarity(
     """
     Compute similarity metric from task 551.
     """
-    return (0, 0)
+    sum = 0
+    divisor = size * (size - 1)
+    for i in range(size):
+        for j in range(i):
+            sum += int(
+                (h1_classes[i] == h1_classes[j]) == (h2_classes[i] == h2_classes[j])
+            )
+    gcd_val = gcd(2 * sum, divisor)
+    return (2 * sum // gcd_val, divisor // gcd_val)
 
 
 if __name__ == "__main__":
     path = Path("./data/551.txt")
     res = similarity(*parser(path))
+    print(f"{res[0]}/{res[1]}")
