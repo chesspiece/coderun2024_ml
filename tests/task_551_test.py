11938d2e8495147e677e619fbe14f0d995cd20d0
Manshilin Oleg <manshilin.o@gmail.com>
2024-07-07 15:10:52 +0300

    Fixed tests

--- tests/task_551_test.py
+++ tests/task_551_test.py
@@ -1,39 +1,37 @@
 from pathlib import Path
 
-import numpy as np
-
 from src.task_551 import similarity, parser
 
 
 def test_parser_551():
     path = Path("./tests/data/551/test1.txt")
     h1_classes, h2_classes, size = parser(path)
-    assert np.all(h1_classes == np.array([1, 2, 0]))
-    assert np.all(h2_classes == np.array([0, 2, 1]))
+    assert h1_classes == [1, 2, 0]
+    assert h2_classes == [0, 2, 1]
     assert size == 3
 
     path = Path("./tests/data/551/test2.txt")
     h1_classes, h2_classes, size = parser(path)
-    assert np.all(h1_classes == np.array([1, 1, 2, 2, 2]))
-    assert np.all(h2_classes == np.array([3, 3, 3, 4, 4]))
+    assert h1_classes == [1, 1, 2, 2, 2]
+    assert h2_classes == [3, 3, 3, 4, 4]
     assert size == 5
 
     path = Path("./tests/data/551/test3.txt")
     h1_classes, h2_classes, size = parser(path)
-    assert np.all(h1_classes == np.array([1, 2, 1, 2, 1, 2, 1, 2, 1, 2]))
-    assert np.all(h2_classes == np.array([1, 2, 3, 4, 5, 5, 4, 3, 2, 1]))
+    assert h1_classes == [1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
+    assert h2_classes == [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
     assert size == 10
 
     path = Path("./tests/data/551/test4.txt")
     h1_classes, h2_classes, size = parser(path)
-    assert np.all(h1_classes == np.array([1, 1]))
-    assert np.all(h2_classes == np.array([0, 0]))
+    assert h1_classes == [1, 1]
+    assert h2_classes == [0, 0]
     assert size == 2
 
     path = Path("./tests/data/551/test5.txt")
     h1_classes, h2_classes, size = parser(path)
-    assert np.all(h1_classes == np.array([1, 1]))
-    assert np.all(h2_classes == np.array([0, 1]))
+    assert h1_classes == [1, 1]
+    assert h2_classes == [0, 1]
     assert size == 2
 
 
