bac9ccbae749c93cd62dabc8b08908b8a405a4e0
Manshilin Oleg <manshilin.o@gmail.com>
2024-07-07 15:02:18 +0300

    Implemented search with binary search

--- src/task_551.py
+++ src/task_551.py
@@ -1,9 +1,16 @@
-import numpy as np
 import os
 from pathlib import Path
-import numpy.typing as npt
 from collections import defaultdict
+import bisect
 
+
+def fisl(elem, sorted_list):
+    # https://docs.python.org/3/library/bisect.html
+    'Locate the leftmost value exactly equal to x'
+    i = bisect.bisect_left(sorted_list, elem)
+    if i != len(sorted_list) and sorted_list[i] == elem:
+        return i
+    return -1
 
 def gcd(a: int, b: int) -> int:
     """
@@ -50,23 +57,25 @@ def similarity(
 
     # sm2_dict: dict[int, int] = defaultdict(lambda: 0)
     divisor = size * (size - 1) >> 1
-    for i in range(0, size):
-        h1_dict[h1_classes[i]].append(i)
-        h2_dict[h2_classes[i]].append(i)
+    #for i in range(0, size):
+    h1_dict[h1_classes[0]].append(0)
+    h2_dict[h2_classes[0]].append(0)
 
     dict_idx1[h1_classes[0]] += 1
     dict_idx2[h2_classes[0]] += 1
     for i in range(1, size):
+        h1_dict[h1_classes[i]].append(i)
+        h2_dict[h2_classes[i]].append(i)
         res1 = h1_dict[h1_classes[i]]
         res2 = h2_dict[h2_classes[i]]
-        # res1 = res1[res1 < i]
-        t1 = dict_idx1[h1_classes[i]]  # t1 = res1.searchsorted(i)
+    
+        t1 = dict_idx1[h1_classes[i]]
         dict_idx1[h1_classes[i]] += 1
         res1 = res1[0:t1]
-        # res2 = res2[res2 < i]
-        t1 = dict_idx2[h2_classes[i]]  # t1 = res1.searchsorted(i)
+        t1 = dict_idx2[h2_classes[i]]
         dict_idx2[h2_classes[i]] += 1
         res2 = res2[0:t1]
+
         sum += i
         if len(res1) == 0:
             sum -= len(res2)
@@ -79,7 +88,9 @@ def similarity(
             sm = 0
             for i in res1:
                 # tts = res2.searchsorted(i)
-                if i not in res2:
+                # if i not in res2:
+                if fisl(i, res2) == -1:
+
                     # if tts==len(res2) or res2[tts] != i:
                     sm += 1
             sum -= sm + (len(res2) - (len(res1) - sm))
@@ -87,7 +98,8 @@ def similarity(
             sm = 0  # sm2_dict[(h2_classes[i], h1_classes[i])]
             for i in res2:
                 # tts = res1.searchsorted(i)
-                if i not in res1:
+                #if i not in res1:
+                if fisl(i, res1) == -1:
                     # if tts==len(res1) or res1[tts] != i:
                     sm += 1
             sum -= sm + (len(res1) - (len(res2) - sm))
