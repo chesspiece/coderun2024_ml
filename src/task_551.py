51aff0117b459e1d389423631aad88dc7922edb3
Manshilin Oleg <manshilin.o@gmail.com>
2024-07-11 02:16:03 +0300

    Workin solution. All tests were passed

--- src/task_551.py
+++ src/task_551.py
@@ -1,17 +1,33 @@
 import os
 from pathlib import Path
-import bisect
 from fractions import Fraction
 from collections import defaultdict
 
 
-def fisl(elem: int, sorted_list: list[int]):
-    # https://docs.python.org/3/library/bisect.html
-    "Locate the leftmost value exactly equal to x"
-    i = bisect.bisect_left(sorted_list, elem)
-    if i != len(sorted_list) and sorted_list[i] == elem:
-        return i
-    return -1
+def countingSort(array: list[int]):
+    size = len(array)
+    output = [0] * size
+
+    # Initialize count array
+    count = [0] * 10
+
+    # Store the count of each elements in count array
+    for i in range(0, size):
+        count[array[i]] += 1
+
+    # Store the cummulative count
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+
+    # Find the index of each element of the original array in count array
+    # place the elements in output array
+    i = size - 1
+    while i >= 0:
+        output[count[array[i]] - 1] = i
+        count[array[i]] -= 1
+        i -= 1
+
+    return output
 
 
 def gcd(a: int, b: int):
@@ -24,7 +40,7 @@ def gcd(a: int, b: int):
     return a
 
 
-def gcd2(a: int, b: int):
+def gcd2(a: int, b: int) -> int:
     if a == b:
         return a
 
@@ -41,20 +57,20 @@ def gcd2(a: int, b: int):
     if (~a & 1) == 1:
         # b is odd
         if (b & 1) == 1:
-            return gcd(a >> 1, b)
+            return gcd2(a >> 1, b)
         else:
             # both a and b are even
-            return gcd(a >> 1, b >> 1) << 1
+            return gcd2(a >> 1, b >> 1) << 1
 
     # a is odd, b is even
     if (~b & 1) == 1:
-        return gcd(a, b >> 1)
+        return gcd2(a, b >> 1)
 
     # reduce larger number
     if a > b:
-        return gcd((a - b) >> 1, b)
+        return gcd2((a - b) >> 1, b)
 
-    return gcd((b - a) >> 1, a)
+    return gcd2((b - a) >> 1, a)
 
 
 def parser(
@@ -79,46 +95,36 @@ def similarity(h1_classes: list[int], h2_classes: list[int], size: int) -> Fract
     Compute similarity metric from task 551.
     """
 
-    summ = 0
-    h1_dict: dict[int, list[int]] = [[] for _ in range(size)]  # type: ignore
-    h2_dict: dict[int, list[int]] = [[] for _ in range(size)]  # type: ignore
-    res2_dict_check: list[dict[int, bool]] = [
-        defaultdict(lambda: False) for _ in range(size)
-    ]
-    # h2_dict_check: list[list[int]] = [[False]*size for _ in range(size)]
-    # sm1_dict: dict[tuple[int, int], tuple[int, int]] = defaultdict(lambda: (0, 0))
-    # sm2_dict: dict[tuple[int, int], tuple[int, int]] = defaultdict(lambda: (0, 0))
+    summ = size * (size - 1) >> 1
+    h1_dict: list[list[int]] = [[] for _ in range(size)]
+    sm1_dict: dict[int, tuple[int, int]] = defaultdict(lambda: (0, 0))
 
-    divisor = size * (size - 1) >> 1
-    h1_dict[h1_classes[0]].append(0)
-    h2_dict[h2_classes[0]].append(0)
-    res2_dict_check[h2_classes[0]][0] = True
+    idxs = countingSort(h2_classes)
+
+    divisor = summ
+    h1_dict[h1_classes[idxs[0]]].append(0)
+    idx_check = 0
 
     for i in range(1, size):
-        r1 = h1_classes[i]
-        r2 = h2_classes[i]
+        ic = idxs[i]
+        r1 = h1_classes[ic]
+        if h2_classes[ic] != h2_classes[idxs[i - 1]]:
+            idx_check = i
+            sm1_dict = defaultdict(lambda: (0, 0))
         res1 = h1_dict[r1]
-        res2 = h2_dict[r2]
         lres1 = len(res1)
-        lres2 = len(res2)
+        lres2 = i - idx_check
 
-        summ += i
-
-        # sm, it = sm1_dict[(h1_classes[i], h2_classes[i])]
-        sm = 0
-        # for ii in res1:#res1[it::]:
-        #        #it += 1
-        #    if not res2_dict_check[r2][ii]:#fisl(ii, res2) == -1:
-        #        sm += 1
-        sm = sum([2 if (fisl(ii, res2) == -1) else 0 for ii in res1])
-        # sm1_dict[(h1_classes[i], h2_classes[i])] = (sm, it)
+        sm, it = sm1_dict[r1]
+        for ii in res1[it::]:
+            it += 1
+            if ii >= idx_check:
+                break
+            sm += 2
+        sm1_dict[r1] = (sm, it)
         summ -= sm + lres2 - lres1
 
         h1_dict[r1].append(i)
-        h2_dict[r2].append(i)
-        res2_dict_check[r2][i] = True
-
-    # gcd_val = gcd(sum, divisor)
     return Fraction(summ, divisor)
 
 
