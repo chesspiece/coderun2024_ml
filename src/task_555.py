bc21cb03bea9fe7522622e8d473ac3ec9d72becd
Manshilin Oleg <manshilin.o@gmail.com>
2024-07-13 20:47:36 +0300

    Created tests for task 555

--- src/task_555.py
+++ src/task_555.py
@@ -1,4 +1,48 @@
+import os
+import pathlib
+
 import numpy as np
+import numpy.typing as npt
+
+
+def parser(
+    path: os.PathLike[str],
+) -> tuple[tuple[int, int, int], npt.NDArray[np.int32], npt.NDArray[np.int32]]:
+    """
+    Parse input from file into an array and kernel
+    """
+    with open(path, "r") as f:
+        n, m, k = (int(x) for x in f.readline().strip().split(sep=" "))
+
+        initial_matrix = np.zeros(
+            (
+                n,
+                m,
+            ),
+            dtype=np.int32,
+        )
+        for initial_matrix_row in range(n):
+            initial_matrix[initial_matrix_row, :] = np.array(
+                [int(x) for x in f.readline().strip().split(sep=" ")]
+            )
+        convolved_matrix = np.zeros(
+            (
+                n - k + 1,
+                m - k + 1,
+            ),
+            dtype=np.int32,
+        )
+        for convolved_matrix_row in range(n - k + 1):
+            convolved_matrix[convolved_matrix_row, :] = np.array(
+                [int(x) for x in f.readline().strip().split(sep=" ")]
+            )
+
+    return (n, m, k), initial_matrix, convolved_matrix
+
+
+def custom_inverse_conv2d():
+    pass
+
 
 if __name__ == "__main__":
     pass
---
+++ tests/task_555_test.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+
+import numpy as np
+
+from src.task_555 import custom_inverse_conv2d, parser
+
+
+def test_parser_555():
+    path = Path("./tests/data/555/test1.txt")
+    params, initial_matrix, result_matrix = parser(path)
+    assert params == (5, 6, 1)
+    assert np.all(
+        initial_matrix
+        == np.array(
+            [
+                [1, 2, 3, 4, 5, 6],
+                [1, 2, 3, 4, 5, 6],
+                [1, 2, 3, 4, 5, 6],
+                [1, 2, 3, 4, 5, 6],
+                [1, 2, 3, 4, 5, 6],
+            ]
+        )
+    )
+    assert np.all(
+        result_matrix
+        == np.array(
+            [
+                [1, 2, 3, 4, 5, 6],
+                [1, 2, 3, 4, 5, 6],
+                [1, 2, 3, 4, 5, 6],
+                [1, 2, 3, 4, 5, 6],
+                [1, 2, 3, 4, 5, 6],
+            ]
+        )
+    )
+
+    path = Path("./tests/data/555/test2.txt")
+    params, initial_matrix, result_matrix = parser(path)
+    assert params == (5, 5, 2)
+    assert np.all(
+        initial_matrix
+        == np.array(
+            [
+                [-48, -47, -5, 58, -93],
+                [35, -26, 42, -58, -59],
+                [-37, 30, 20, 100, -83],
+                [43, -99, -9, 19, -48],
+                [93, 37, -84, -99, 84],
+            ]
+        )
+    )
+    assert np.all(
+        result_matrix
+        == np.array(
+            [
+                [-2688, 10345, -6343, -9836],
+                [1671, 3474, 1850, -13016],
+                [-8449, 5224, 4502, -18620],
+                [-11835, -2233, -1057, 12591],
+            ]
+        )
+    )
+
+    path = Path("./tests/data/555/test3.txt")
+    params, initial_matrix, result_matrix = parser(path)
+    assert params == (6, 5, 3)
+    assert np.all(
+        initial_matrix
+        == np.array(
+            [
+                [-17, -37, 61, -19, 90],
+                [94, 6, 22, 86, -82],
+                [10, 65, -56, 52, -80],
+                [-52, -53, -27, -81, 39],
+                [4, 22, 58, -53, -63],
+                [-98, 62, -100, 66, -34],
+            ]
+        )
+    )
+    assert np.all(
+        result_matrix
+        == np.array(
+            [
+                [-375, 16185, -6904],
+                [-9850, -11424, 5127],
+                [2151, -14707, 1641],
+                [-1387, 10203, -15056],
+            ]
+        )
+    )
