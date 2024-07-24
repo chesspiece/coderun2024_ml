d5be9f2cec0d0356521b4d04bb16a6507df2764f
Manshilin Oleg <manshilin.o@gmail.com>
2024-07-25 00:24:07 +0300

    Tests for task 552 main function are created

--- tests/task_552_test.py
+++ tests/task_552_test.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from src.task_552 import parser
+from src.task_552 import check_tandem, parser
 
 
 def test_parser_552():
@@ -15,3 +15,17 @@ def test_parser_552():
     path = Path("./tests/data/552/test3.txt")
     input_strigs = parser(path)
     assert input_strigs == ["a", "aa", "aaa", "aaaa"]
+
+
+def test_task_552():
+    path = Path("./tests/data/552/test1.txt")
+    res = check_tandem(parser(path))
+    assert res == [(2, 3), (3, 2)]
+
+    path = Path("./tests/data/552/test2.txt")
+    res = check_tandem(parser(path))
+    assert res == [(1, 4), (2, 3), (3, 2), (4, 1)]
+
+    path = Path("./tests/data/552/test3.txt")
+    res = check_tandem(parser(path))
+    assert res == [(1, 3), (2, 4), (3, 1), (4, 2)]
