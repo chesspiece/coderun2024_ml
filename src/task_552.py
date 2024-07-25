a58472552f3998487d9628359e26f256b30bcb08
Manshilin Oleg <manshilin.o@gmail.com>
2024-07-26 01:33:57 +0300

    First baseline version of task 552 is implemented

--- src/task_552.py
+++ src/task_552.py
@@ -14,9 +14,25 @@ def parser(
 
 
 def check_tandem(input_strings: list[str]) -> list[tuple[int, int]]:
-    return [(0, 1)]
+    res: list[tuple[int, int]] = []
+    for idx1, str1 in enumerate(input_strings):
+        for idx2, str2 in enumerate(input_strings):
+            if idx1 == idx2:
+                continue
+            new_str = str1 + str2
+            if len(new_str) % 2 != 0:
+                continue
+            half_len = len(new_str) // 2
+            for i in range(half_len):
+                if new_str[i] != new_str[i + half_len]:
+                    break
+            else:
+                res.append((idx1 + 1, idx2 + 1))
+    return res
 
 
 if __name__ == "__main__":
     path = Path("./data/552.txt")
     res = check_tandem(parser(path))
+    for i, j in res:
+        print(f"{i} {j}")
