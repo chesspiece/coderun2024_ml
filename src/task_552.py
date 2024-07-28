c541e77c58b5d50a8fe5542e40f221659f9b78b6
Manshilin Oleg <manshilin.o@gmail.com>
2024-07-28 15:38:53 +0300

    Faster solution. Task 552 is solved.

--- src/task_552.py
+++ src/task_552.py
@@ -1,4 +1,5 @@
 import os
+from collections import defaultdict
 from pathlib import Path
 
 
@@ -15,20 +16,25 @@ def parser(
 
 def check_tandem(input_strings: list[str]) -> list[tuple[int, int]]:
     res: list[tuple[int, int]] = []
+    add_str_left: dict[str, list[int]] = defaultdict(lambda: [])
     for idx1, str1 in enumerate(input_strings):
-        for idx2, str2 in enumerate(input_strings):
-            if idx1 == idx2:
+        add_str_left[str1].append(idx1)
+        ln = len(str1)
+        ln2 = ln // 2
+        for i in range(ln2):
+            if str1[0 : i + 1] == str1[ln - i - 1 : :]:
+                add_str_left[str1[i + 1 : ln - i - 1]].append(idx1)
+    for idx1, str1 in enumerate(input_strings):
+        idx_lst = add_str_left[str1]
+        for idx in idx_lst:
+            if idx == idx1:
                 continue
-            new_str = str1 + str2
-            if len(new_str) % 2 != 0:
+            res.append((idx + 1, idx1 + 1))
+        for idx in idx_lst:
+            if idx == idx1:
                 continue
-            half_len = len(new_str) // 2
-            for i in range(half_len):
-                if new_str[i] != new_str[i + half_len]:
-                    break
-            else:
-                res.append((idx1 + 1, idx2 + 1))
-    return res
+            res.append((idx1 + 1, idx + 1))
+    return sorted(res)
 
 
 if __name__ == "__main__":
